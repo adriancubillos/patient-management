f3f2b7db1c513626b2cb8f6037507ecb241b1142
Adrian Cubillos <adriancubillos@gmail.com>
2025-08-29 20:08:45 -0400

    Adding Update patient to service, controller and exceptions

--- patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -28,7 +28,16 @@ public class GlobalExceptionHandler {
 
         log.warn("Email address already exists: {}", ex.getMessage());
         Map<String, String> errors = new HashMap<>();
-        errors.put("email", "Email address already exists");
+        errors.put("message", "Email address already exists");
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(PatientNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePatientNotFoundException(PatientNotFoundException ex) {
+
+        log.warn("Patient not found: {}", ex.getMessage());
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "Patient not found");
         return ResponseEntity.badRequest().body(errors);
     }
 }
