58cf9f024e6a4afc905f2904ac9f301739bbf8a3
Adrian Cubillos <adriancubillos@gmail.com>
2025-09-04 16:36:34 -0400

    created Virtual Private Cloud

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,11 +1,23 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class LocalStack extends Stack {
 
+    private final Vpc vpc;
+
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
+
+        this.vpc = createVpc();
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder.create(this, "PatientManagementVpc")
+                .vpcName("PatientManagementVpc")
+                .maxAzs(2)  // Default is all AZs (avaliability zones) in the region
+                .build();
     }
 
     public static void main(final String[] args) {
